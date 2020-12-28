898d026c0eb4c57ff9843813b2a8a829dfe436ff
dedy <dedy@apjati.apjati>
2020-12-28 06:34:30 -0500

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laratrust\Traits\LaratrustUserTrait;
+use App\Traits\Uuid;
 
 class User extends Authenticatable
 {
+    use Uuid;
+    use LaratrustUserTrait;
     use HasFactory, Notifiable;
 
     /**
